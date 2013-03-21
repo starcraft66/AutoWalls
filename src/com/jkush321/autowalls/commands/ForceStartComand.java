37720bb13b2a0e0fe747dc5471344f53a97be03e
starcraft66 <starcraft66@gmail.com>
2013-03-21 08:18:16 -0400

    Completely restructured commands into seperate classes!

---
+++ src/com/jkush321/autowalls/commands/ForceStartComand.java
@@ -0,0 +1,11 @@
+package com.jkush321.autowalls.commands;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: susanna
+ * Date: 13-03-20
+ * Time: 7:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ForceStartComand {
+}
