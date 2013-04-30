166d9a9dbd508dcc8a073267244ec3e30fe7b9aa
Tristan Gosselin-Hane <starcraft66@gmail.com>
2013-04-30 08:52:47 -0400

    Show No permission instead of usage when not admin

--- src/com/jkush321/autowalls/commands/PrefixCommand.java
+++ src/com/jkush321/autowalls/commands/PrefixCommand.java
@@ -18,59 +18,71 @@ public class PrefixCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender cmdSender, Command cmd, String label, String[] args) {
 
-        if (args.length < 2) {
-            cmdSender.sendMessage(ChatColor.RED + "Usage : /prefix <set | remove> <name> <amount>");
-            return false;
-        } else {
-
-            String playerName = "";
-            if (Bukkit.getPlayer(args[1]) == null || !Bukkit.getPlayer(args[1]).isOnline()) {
-                playerName = args[1];
+        if (cmdSender.isOp() || cmdSender.hasPermission("walls.op")) {
+            if (args.length < 2) {
+                cmdSender.sendMessage(ChatColor.RED + "Usage : /prefix <set | remove> <name> <prefix>");
+                return false;
             } else {
-                playerName = Bukkit.getPlayer(args[1]).getName();
-            }
-            String fullPrefix = "";
-            for (int i = 2; i < args.length; i++) {
-                if (i > 0) {
-                    fullPrefix += args[i] + " ";
-                }
-            }
-            fullPrefix = fullPrefix.trim();
 
-            if (args[0].equalsIgnoreCase("set")) {
-                plugin.config.set("prefix." + playerName, fullPrefix);
-                plugin.saveConfig();
-                if (Bukkit.getPlayer(playerName).isOnline() && Bukkit.getPlayer(playerName) != null) {
-                    if (plugin.config.isSet("prefix." + Bukkit.getPlayer(playerName).getName())) {
-                        Bukkit.getPlayer(playerName).setDisplayName(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("prefix." + Bukkit.getPlayer(playerName).getName()).replace("{pri}", plugin.config.getInt("votes.players." + Bukkit.getPlayer(playerName).getName()) + "") + Bukkit.getPlayer(playerName).getName() + ChatColor.WHITE));
+
+                String playerName = "";
+                if (Bukkit.getPlayer(args[1]) == null || !Bukkit.getPlayer(args[1]).isOnline()) {
+                    playerName = args[1];
+                } else {
+                    playerName = Bukkit.getPlayer(args[1]).getName();
+                }
+                String fullPrefix = "";
+                for (int i = 2; i < args.length; i++) {
+                    if (i > 0) {
+                        fullPrefix += args[i] + " ";
                     }
                 }
-                cmdSender.sendMessage(ChatColor.YELLOW + "Set " + playerName + "'s prefix to " + ChatColor.WHITE + "\"" + fullPrefix + ChatColor.WHITE + "\"");
-                return true;
-            }
+                fullPrefix = fullPrefix.trim();
 
-            if (args[0].equalsIgnoreCase("remove")) {
-
-                if (plugin.config.getBoolean("priorities") == true) {
-                    if (Bukkit.getPlayer(playerName).isOnline()) {
-                        plugin.getConfig().set("prefix." + playerName, null);
-                        plugin.saveConfig();
-                        cmdSender.sendMessage(ChatColor.YELLOW + "Removed" + ChatColor.WHITE + playerName + ChatColor.YELLOW + "'s prefix");
-                        return true;
+                if (args[0].equalsIgnoreCase("set")) {
+                    plugin.config.set("prefix." + playerName, fullPrefix);
+                    plugin.saveConfig();
+                    if (Bukkit.getPlayer(playerName) != null && Bukkit.getPlayer(playerName).isOnline()) {
+                        if (plugin.config.isSet("prefix." + Bukkit.getPlayer(playerName).getName())) {
+                            Bukkit.getPlayer(playerName).setDisplayName(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("prefix." + Bukkit.getPlayer(playerName).getName()).replace("{pri}", plugin.config.getInt("votes.players." + Bukkit.getPlayer(playerName).getName()) + "") + Bukkit.getPlayer(playerName).getName() + ChatColor.WHITE));
+                        }
                     }
-                }
-                if (Bukkit.getPlayer(playerName).hasPermission("walls.op") || cmdSender.isOp()) {
-                    Bukkit.getPlayer(playerName).setDisplayName(ChatColor.DARK_BLUE + "[" + ChatColor.DARK_GREEN + "Admin" + ChatColor.DARK_BLUE + "]" + ChatColor.DARK_RED + Bukkit.getPlayer(playerName).getName() + ChatColor.GRAY + ChatColor.WHITE);
-                }
-                if (plugin.config.isSet("prefix." + Bukkit.getPlayer(playerName).getName())) {
-                    Bukkit.getPlayer(playerName).setDisplayName(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("prefix." + Bukkit.getPlayer(playerName).getName())).replace("{pri}", plugin.config.getInt("votes.players." + Bukkit.getPlayer(playerName).getName()) + "") + Bukkit.getPlayer(playerName).getName() + ChatColor.WHITE);
+                    cmdSender.sendMessage(ChatColor.YELLOW + "Set " + playerName + "'s prefix to " + ChatColor.WHITE + "\"" + fullPrefix + ChatColor.WHITE + "\"");
                     return true;
                 }
-                //impossible
-                return true;
+
+                if (args[0].equalsIgnoreCase("remove")) {
+
+                    if (plugin.config.getBoolean("priorities") == true) {
+
+                        plugin.getConfig().set("prefix." + playerName, null);
+                        plugin.saveConfig();
+                        cmdSender.sendMessage(ChatColor.YELLOW + "Removed " + ChatColor.WHITE + playerName + ChatColor.YELLOW + "'s prefix");
+
+                        if (Bukkit.getPlayer(playerName) != null && Bukkit.getPlayer(playerName).isOnline()) {
+                            if (Bukkit.getPlayer(playerName).hasPermission("walls.op") || cmdSender.isOp()) {
+                                Bukkit.getPlayer(playerName).setDisplayName(ChatColor.DARK_BLUE + "[" + ChatColor.DARK_GREEN + "Admin" + ChatColor.DARK_BLUE + "]" + ChatColor.DARK_RED + Bukkit.getPlayer(playerName).getName() + ChatColor.GRAY + ChatColor.WHITE);
+                                return true;
+                            }
+                            if (plugin.config.isSet("prefix." + Bukkit.getPlayer(playerName).getName())) {
+                                Bukkit.getPlayer(playerName).setDisplayName(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("prefix." + Bukkit.getPlayer(playerName).getName())).replace("{pri}", plugin.config.getInt("votes.players." + Bukkit.getPlayer(playerName).getName()) + "") + Bukkit.getPlayer(playerName).getName() + ChatColor.WHITE);
+                                return true;
+                            }
+                            //impossible
+                            return true;
+                        } else {
+                            return true;
+                        }
+                    }
+                }
+
+                cmdSender.sendMessage(ChatColor.RED + "Usage : /prefix <set | remove> <name> <prefix>");
+                return false;
+
             }
-            cmdSender.sendMessage(ChatColor.RED + "Usage : /prefix <set | remove> <name> <prefix>");
-            return false;
+        } else {
+            cmdSender.sendMessage(ChatColor.RED + "No permission!");
+            return true;
         }
     }
 }
