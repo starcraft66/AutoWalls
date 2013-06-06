95824abc73ed8519bbdab31a30e5b6fa1ebfba65
Tristan Gosselin <starcraft66@gmail.com>
2013-06-06 18:56:18 -0400

    Added /priority <name> add <priority> to add priorityto a user.

--- src/com/jkush321/autowalls/commands/PriorityCommand.java
+++ src/com/jkush321/autowalls/commands/PriorityCommand.java
@@ -46,14 +46,14 @@ public class PriorityCommand implements CommandExecutor {
 
             if (args.length == 3 && args[1].equalsIgnoreCase("set")) {
 
-                int priority = Integer.parseInt(args[2]);
+                int prioritySet = Integer.parseInt(args[2]);
 
                 if (Bukkit.getPlayer(args[0]) != null) {
 
                     Player pl = Bukkit.getPlayer(args[0]);
 
 
-                    plugin.config.set("votes.players." + pl.getName(), priority);
+                    plugin.config.set("votes.players." + pl.getName(), prioritySet);
                     plugin.saveConfig();
                     if (pl.isOnline()) {
                         Bukkit.getPlayer(pl.getName()).sendMessage(ChatColor.YELLOW + "Your priority is now " + plugin.config.getInt("votes.players." + pl.getName()));
@@ -79,7 +79,48 @@ public class PriorityCommand implements CommandExecutor {
 
                     String pl = args[0];
 
-                    plugin.config.set("votes.players." + pl, priority);
+                    plugin.config.set("votes.players." + pl, prioritySet);
+                    plugin.saveConfig();
+                    cmdSender.sendMessage(ChatColor.YELLOW + pl + "'s priority is now " + plugin.config.getInt("votes.players." + pl));
+
+                    return true;
+                }
+            } else if (args.length == 3 && args[1].equalsIgnoreCase("add")) {
+
+                int priorityAdd = Integer.parseInt(args[2]);
+
+                if (Bukkit.getPlayer(args[0]) != null) {
+
+                    Player pl = Bukkit.getPlayer(args[0]);
+
+
+                    plugin.config.set("votes.players." + pl.getName(), (plugin.config.getInt("votes.players." + pl.getName()) + priorityAdd));
+                    plugin.saveConfig();
+                    if (pl.isOnline()) {
+                        Bukkit.getPlayer(pl.getName()).sendMessage(ChatColor.YELLOW + "Your priority is now " + plugin.config.getInt("votes.players." + pl.getName()));
+                    }
+                    cmdSender.sendMessage(ChatColor.YELLOW + pl.getName() + "'s priority is now " + plugin.config.getInt("votes.players." + pl.getName()));
+                    if (Bukkit.getPlayer(pl.getName()).hasPermission("walls.op") || pl.isOp()) {
+                        Bukkit.getPlayer(pl.getName()).setDisplayName(ChatColor.DARK_BLUE + "[" + ChatColor.DARK_GREEN + "Admin" + ChatColor.DARK_BLUE + "]" + ChatColor.DARK_RED + Bukkit.getPlayer(pl.getName()).getName() + ChatColor.GRAY + ChatColor.WHITE);
+                        return true;
+                    }
+                    if (plugin.config.isSet("votes.players." + pl.getName()) && plugin.config.getInt("votes.players." + pl.getName()) >= 20) {
+                        pl.setDisplayName(ChatColor.DARK_AQUA + pl.getName() + ChatColor.WHITE);
+                    }
+                    if (plugin.config.isSet("votes.players." + pl.getName()) && plugin.config.getInt("votes.players." + pl.getName()) >= 250) {
+                        pl.setDisplayName(ChatColor.DARK_RED + pl.getName() + ChatColor.WHITE);
+                    }
+                    if (plugin.config.getBoolean("priorities") == true) {
+                        if (plugin.config.isSet("votes.players." + pl.getName())) {
+                            pl.setDisplayName(ChatColor.YELLOW + "[" + plugin.config.getInt("votes.players." + pl.getName()) + "]" + ChatColor.GRAY + pl.getDisplayName() + ChatColor.WHITE);
+                        } else pl.setDisplayName(ChatColor.GRAY + "[0]" + pl.getDisplayName() + ChatColor.WHITE);
+                    }
+                    return true;
+                } else {
+
+                    String pl = args[0];
+
+                    plugin.config.set("votes.players." + pl, (plugin.config.getInt("votes.players." + pl + priorityAdd)));
                     plugin.saveConfig();
                     cmdSender.sendMessage(ChatColor.YELLOW + pl + "'s priority is now " + plugin.config.getInt("votes.players." + pl));
 
